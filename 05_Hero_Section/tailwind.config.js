40719663f1a2f6c372706c6825f81949e068c407
k1r03478 <dipaksharma34447255@gmail.com>
2023-01-31 22:51:15 +0545

    Simple Hero Section

---
+++ 05_Hero_Section/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    fontFamily: {
+      serif: ['Crimson Text', 'serif'],
+      sans: ['DM Sans', 'sans-serif']
+    },
+    extend: {
+      colors: {
+        herowine: "#92003B",
+        herolight: "#FF7BE5"
+      }
+    },
+  },
+  plugins: [],
+}
