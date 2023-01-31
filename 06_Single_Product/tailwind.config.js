1880985822c19f8e5d7d4c8fbb3d0ec7b5084e36
k1r03478 <dipaksharma34447255@gmail.com>
2023-02-01 00:48:25 +0545

    Initial Commit

---
+++ 06_Single_Product/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {
+      colors: {
+        pgreen: "#788570",
+      }
+    },
+  },
+  plugins: [],
+}
